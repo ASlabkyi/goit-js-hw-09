c38db78c45e67cd32fba954948797b9f01cd2832
Andrii Slabkyi <aslabkyi@gmail.com>
2023-02-19 18:22:16 +0200

    Complete task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnEls = document.querySelectorAll('button');
+const startBtnEl = btnEls[0];
+const closeBtnEl = btnEls[1];
+const NOTIFICATION_DELAY = 1000;
+let changeColorInterval = null;
 
+function handleChangeColor() {
+  if (changeColorInterval) {
+    return;
+  }
+
+  changeColorInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, NOTIFICATION_DELAY);
+}
+
+function handleStopingChangeColor() {
+  clearInterval(changeColorInterval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtnEl.addEventListener('click', handleChangeColor);
+closeBtnEl.addEventListener('click', handleStopingChangeColor);
