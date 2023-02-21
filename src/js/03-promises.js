0ca6f32d7d9bed0d5b4b8a3cca2cd2785dc1883c
Andrii Slabkyi <aslabkyi@gmail.com>
2023-02-21 15:46:45 +0200

    Complete task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        return resolve({ position, delay });
+      } else {
+        return reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  const { target } = e;
+
+  const delay = target.delay.value;
+  const step = target.step.value;
+  const amount = target.amount.value;
+
+  resultPromise(delay, step, amount);
+}
+
+function resultPromise(delay, step, amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const delayWithStep = Number(delay) + i * step;
+
+    createPromise(i + 1, delayWithStep)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+formEl.addEventListener('submit', handleFormSubmit);
